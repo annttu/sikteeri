1bfc7bb4f51eda147a5ce3b76d08be50cb093fb7
Antti Jaakkola <annttu@kapsi.fi>
2012-08-14 16:34:54 +0300

    Itella's information service import tool

---
+++ membership/management/commands/changed_addresses.py
@@ -0,0 +1,108 @@
+# encoding: UTF-8
+
+import os
+import logging
+import struct
+import StringIO
+from membership.models import Contact
+from membership.utils import log_change
+
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
+
+logger = logging.getLogger("membership.changed_addresses")
+
+def Parser(filehandle):
+    """Parser generator"""
+    # TODO: support for person who has moved to abroad
+    if type(filehandle) == type(""):
+        filehandle = StringIO.StringIO(filehandle)
+        print('stringio')
+    personfields = (11, 3, 8, 1, 70, 30, 25, 4, 1,
+                    8, 5, 30, 71, 30, 5, 30, 71, 30)
+    companyfields = (11, 3, 8, 1, 50, 50, 25, 4, 1,
+                    8, 5, 30, 71, 30, 5, 30, 71, 30)
+    personlabels = ('customer_number', 'service_number', 'saving_date', 
+                        'customer_type', 'name', 'old_last_name', 
+                        'identification_number', 'status', 'transaction',
+                        'address_date', 'new_postal_code', 'new_post_office', 
+                        'new_street_address', 'new_address_family_id', 
+                        'old_postal_code', 'old_post_office', 
+                        'old_street_address', 'old_address_family_id')
+    companylabels = ('customer_number', 'service_number', 'saving_date', 
+                        'customer_type', 'name', 'operating_name', 
+                        'identification_number', 'status', 'transaction',
+                        'address_date', 'new_postal_code', 'new_post_office', 
+                        'new_street_address', 'new_address_family_id', 
+                        'old_postal_code', 'old_post_office', 
+                        'old_street_address', 'old_address_family_id')
+    fmtstring = ''.join('%ds' % f for f in personfields)
+    personparse = struct.Struct(fmtstring).unpack_from
+    fmtstring = ''.join('%ds' % f for f in companyfields)
+    companyparse = struct.Struct(fmtstring).unpack_from
+    for line in filehandle:
+        if len(line) < 197:
+            logger.error('Too short row found!')
+            continue
+        if line[22] == '1':
+            # Person
+            fields = personparse(line)
+            yield fields_to_dict(personlabels, fields)
+        elif line[22] == '2':
+            # Company
+            fields = companyparse(line)
+            yield fields_to_dict(companylabels, fields)
+
+def fields_to_dict(keys, values):
+    retval = {}
+    for key, value in zip(keys, values):
+        retval[key] = " ".join(value.strip().split()
+                              ).decode('ISO-8859-1').encode('utf-8')
+    return retval
+
+def update_contact(record):
+    if 'operating_name' in record:
+        # organization member
+        contacts = Contact.objects.filter(
+                                       organization_name__iexact=record['name'])
+    else:
+        last_name = record['name'].split()[0]
+        first_name = record['name'].split()[1]
+        contacts = Contact.objects.filter(last_name__iexact=last_name,
+                            first_name__iexact=first_name)
+    contacts = contacts.filter(postal_code__exact=record['old_postal_code'],
+                            post_office__iexact=record['old_post_office'],
+                            street_address__iexact=record['old_street_address'])
+    contacts = contacts.all()
+    if len(contacts) == 0:
+        logging.error('Cannot find contact for record %s' % record['name'])
+        return
+    for contact in contacts:
+        before = contact.__dict__.copy()
+        contact.street_address = record['new_street_address']
+        contact.postal_code = record['new_postal_code']
+        contact.post_office = record['new_post_office']
+        print('save')
+        contact.save()
+        after = contact.__dict__.copy()
+        log_user = User.objects.get(id=1) # Get sikteeri user
+        log_change(contact, log_user, before, after)
+        logging.info('Updated contact %s' % contact)
+    return True
+    
+
+def changed_addresses(filehandle):
+    for record in Parser(filehandle):
+       update_contact(record) 
+
+
+class Command(BaseCommand):
+    args = '<file1> [<file2>] ...'
+    help = '''Import changed addresses from Itella's information service'''
+    
+    def handle(self, *args, **options):
+        for cfile in args:
+            logger.info("Starting the processing of file %s." %
+                os.path.abspath(cfile))
+            f = open(cfile, 'r')
+            changed_addresses(f)
