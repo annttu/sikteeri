f0219795fd20dd4b6175be8309b59b2f81ee5238
Antti Jaakkola <annttu@git.annttu.fi>
2018-05-13 16:07:01 +0300

    Api for membership and contact editing

--- sikteeri/settings.py
+++ sikteeri/settings.py
@@ -204,8 +204,10 @@ INSTALLED_APPS = (
     'django.contrib.humanize',
     'django_comments',
     'django.contrib.staticfiles',
+    'rest_framework',
     'membership',
     'services',
+    'api',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -238,6 +240,17 @@ LOGIN_URL = 'login'
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissions',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
+}
 
 #####################################################
 ## Logging configuration
